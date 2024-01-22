cfde1dfb6072183581d05ef16605658482a7a611
FrancisPicard2000 <francispicar72@gmail.com>
2024-01-22 00:04:53 +0000

    now scrapes the NHL website for draft data

--- scripts/nhl_draft_scraper.py
+++ scripts/nhl_draft_scraper.py
@@ -1,102 +1,122 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import argparse
 import json
 import csv
-from pathlib import Path
-from bs4 import BeautifulSoup
 
 
-# Collect the html content of Elite Prospect NHL Entry Draft for a given year
-def get_draft_page_html_content(year):
+
+# Extract draft's data
+def extract_draft_data(driver):
+
+    draft_data = []
+
+    # Collect data on all web pages
+    while True:
+        page_data_unformatted = retrieve_player_info_page(driver)
+        page_data_formatted = format_raw_data(page_data_unformatted)
+        draft_data += page_data_formatted
+
+        next_page_button = driver.find_element(By.CLASS_NAME, "next-button")
+
+        if (not (next_page_button.is_enabled())):
+            break
+        
+        next_page_button.click()
+        
+
+    return draft_data
+
+
+
+# Retrieve the players' info in the given page
+def retrieve_player_info_page(driver):
     
-    fpath = Path(f'../data/html_pages/draft/draft{year}.html')
-    HEADERS = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Cache-Control": "max-age=0",
-    }
-    
-    # Caching: Only request if we haven't done so before
-    if not fpath.exists():
-        html_data = requests.get(f"https://www.hockeydb.com/ihdb/draft/nhl{year}e.html", headers=HEADERS)
+    data = []
 
-        with open(fpath, "w") as f:
-            f.write(html_data.text)
+    player_blocks = driver.find_elements(By.CLASS_NAME, "rt-tr-group")
+    for player in player_blocks:
+        player_info_components = player.text.split("\n")
+        data.append(player_info_components)
 
-    with open(fpath, "r") as f:
-        return f.read()
+    return data
 
 
 
-# Retrieve the draft data from the html content
-def retrieve_draft_data(html_content):
+# Format heights from imperial to metric (to get continuous-ish data)
+def format_height(height_string):
 
-    soup = BeautifulSoup(html_content, "html.parser")
-    data_body = soup.find("tbody")
-    players_tr_list = data_body.find_all("tr", class_=lambda x: x != "hideme")
+    height_components = height_string.strip("\"").split("\' ")
+    foot = int(height_components[0])
+    inches = int(height_components[1])
 
+    return (30*foot + 2.5*inches)
+
+
+
+# Format the raw data extracted from a webpage
+def format_raw_data(raw_data):
+
+    formatted_data = []
     team_label_map_file = open("../data/other/team_label_map.json", "r")
     team_label_map = json.load(team_label_map_file)
 
-    chl_data = []
-
-    for player_tr in players_tr_list:
-
-        # Extract the name
-        player_name = player_tr.find("a", {"target" : "players"}).text
-
-        # Extract the position
-        player_position_unformatted = player_tr.find("td", {"class" : "hidemob"}).text
-        if (player_position_unformatted in ["L", "R"]):
-            player_position = player_position_unformatted + "W"
+    for raw_player_data in raw_data:
+        if (len(raw_player_data) == 12):
+            add_ind = 0
         else:
-            player_position = player_position_unformatted
-        
-        # Extract the team label
-        player_junior_team_unformatted = player_tr.find("td", {"class" : "l hidemob"}).text
-        player_junior_team_components = player_junior_team_unformatted.split(" ")
-        player_junior_team = " ".join(player_junior_team_components[0:-1])
-        player_junior_team_league = player_junior_team_components[-1].strip("[]")
+            add_ind = 1
 
-        if player_junior_team_league in ["QMJHL", "OHL", "WHL"]:      # If the player is from the CHL, keep his info
-            player_junior_team_label = team_label_map[player_junior_team]
-            chl_data.append([player_name, player_position, player_junior_team_league, player_junior_team_label])
+        if (len(raw_player_data) not in [11, 12]):
+            continue # Missing data (e.g. 2009 draft pick 118 by the Maple Leafs)
 
-    team_label_map_file.close()
-    return chl_data
-            
+        player_name = raw_player_data[1]
+        player_position = raw_player_data[6-add_ind]
+        player_junior_team_league = raw_player_data[10-add_ind]
+        player_height = format_height(raw_player_data[8-add_ind])
+        player_weight = int(raw_player_data[9-add_ind])
+
+        # If the player is from the CHL, keep his info
+        if (player_junior_team_league in ['QMJHL', 'OHL', 'WHL']):
+            player_junior_team_label = team_label_map[raw_player_data[-1]]
+            player_data = [player_name, player_position, player_junior_team_league, player_junior_team_label, player_height, player_weight]
+            formatted_data.append(player_data)
+
+    return formatted_data
+
 
 
 def main():
+
+    # Parse the arguments to retrieve the desired draft year
     parser = argparse.ArgumentParser()
     parser.add_argument("-y", help="year to scrape")
-
     args = parser.parse_args()
     year = int(args.y)
+    
+    # Setup webdriver and access webpage
+    options = webdriver.FirefoxOptions()
+    options.add_argument("-headless")
+    driver = webdriver.Firefox(options=options)
+    driver.get(f"https://records.nhl.com/draft/draft-picks?year={year}")
+    driver.implicitly_wait(5)
 
-    html_content = get_draft_page_html_content(year)
-    draft_data = retrieve_draft_data(html_content)
+    # Retrieve the data
+    data = extract_draft_data(driver)
 
     # Store the draft data
     draft_data_file_path = f"../data/extracted_data/draft/draft{year}_stats.csv"
-    header = ["Name", "Position", "League", "Team"]
+    header = ["Name", "Position", "League", "Team", "Height", "Weight"]
 
     with open(draft_data_file_path, 'w', newline='') as f:
 
         tsv_writer = csv.writer(f, delimiter='\t')
         tsv_writer.writerow(header)
 
-        for datapoint in draft_data:
+        for datapoint in data:
             tsv_writer.writerow(datapoint)
 
     
+
 if __name__ == "__main__":
     main()
